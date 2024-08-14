9b1e184a4647b380a2cd5d88015a09e1f365d268
Harjit Kaur <harjitkaurkhalsa1@gmail.com>
2024-08-14 23:45:56 +0530

    Landscape

---
+++ exercise_fizzbuzz/index.js
@@ -0,0 +1,16 @@
+
+const output=fizzBuzz(false);
+console.log(output);
+
+function fizzBuzz(input){
+    if(typeof input !== 'number')
+        return 'NaN';
+    if((input % 3 ===0 )&& (input % 5 === 0 ))
+        return 'Fizzbuzz';
+    if(input % 3 === 0 )
+        return 'Fizz';
+    if(input % 5 === 0)
+        return 'Buzz';
+
+    return input;
+}
