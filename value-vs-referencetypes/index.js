71013d640b3af30498adbb905e25147b5b244708
Harjit Kaur <harjitkaurkhalsa1@gmail.com>
2024-08-20 21:25:52 +0530

    referencetypes

---
+++ value-vs-referencetypes/index.js
@@ -0,0 +1,19 @@
+// let x=10;
+// let x={value:10};
+// y=x;
+// x.value=20;
+
+// let number=10;
+// function increase(number){
+//     number++;
+// }
+// increase(number);
+// console.log(number);
+
+let obj={value:10};
+function increase(obj){
+    obj.value++;
+}
+increase(obj);
+console.log(obj);
+
